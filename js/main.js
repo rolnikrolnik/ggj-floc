7774dc3c6e6e39e03d479d9ae041b40528132199
Karolus <karol.gierszewski@gmail.com>
2019-01-20 19:11:13 +0100

    Test

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var gane;
+window.onload=function()
+{
+    var config = {
+        type: Phaser.AUTO,
+        width: 480,
+        height: 640,
+        parent: 'phaser-game',
+        scene: [SceneMain]
+    };
+    game = new Phaser.Game(config);
+}
