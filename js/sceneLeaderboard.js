0080ac37201abf1c89bd56c05be8ef2405bda189
Karolus <karol.gierszewski@gmail.com>
2019-01-26 11:59:59 +0100

    pozdro dla kumatych

--- js/sceneLeaderboard.js
+++ js/sceneLeaderboard.js
@@ -7,8 +7,8 @@ class SceneLeaderboard extends Phaser.Scene {
     
     preload()
     {
-    }
 
+    }
     create ()
     {
         this.ranking=this.add.text(500,600,"RANKING",{fontFamily:'ZCOOL KuaiLe',color:'#df7919',fontSize:'40px'});
