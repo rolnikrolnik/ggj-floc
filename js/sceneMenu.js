a6f62a55a3a21fec2d688950f9f641f79231880c
Karolus <karol.gierszewski@gmail.com>
2019-01-27 10:32:46 +0100

    dq

--- js/sceneMenu.js
+++ js/sceneMenu.js
@@ -23,6 +23,8 @@ class SceneMenu extends Phaser.Scene {
         this.load.image('redPipe3', 'images/pipes/red3.png');
         this.load.image('redPipe4', 'images/pipes/red4.png');
         this.load.image('redPipe5', 'images/pipes/red5.png');
+        this.load.image('redPipe6', 'images/pipes/red6.png');
+        this.load.image('redPipe7', 'images/pipes/red7.png');
 
         this.load.image('bluePipe1', 'images/pipes/blue1.png');
         this.load.image('bluePipe2', 'images/pipes/blue2.png');
@@ -30,6 +32,7 @@ class SceneMenu extends Phaser.Scene {
         this.load.image('bluePipe4', 'images/pipes/blue4.png');
         this.load.image('bluePipe5', 'images/pipes/blue5.png');
         this.load.image('bluePipe6', 'images/pipes/blue6.png');
+        this.load.image('bluePipe7', 'images/pipes/blue7.png');
 
     }
 
@@ -44,7 +47,7 @@ class SceneMenu extends Phaser.Scene {
         this.add.text(560,500,"MANUAL",{fontFamily:'ZCOOL KuaiLe',color:'#df7919',fontSize:'40px'});
         this.pressSpacebar = this.add.text(380,700,"Press spacebar to continue...", {fontFamily:'ZCOOL KuaiLe',color:'#df7919',fontSize:'40px'});
 
-        drawPipesInitial();
+        this.drawPipesInitial();
 
         this.spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -83,9 +86,19 @@ class SceneMenu extends Phaser.Scene {
     update() {
         if (Phaser.Input.Keyboard.JustDown(this.cursors.up)) {
             if (this.choice == 0) {
-                this.choice = 2;
+                this.choice = 2;               
             } else {
-                this.choice--;
+                this.choice--;               
+            }
+
+            if(this.choice == 0){
+                this.drawPipesInitial();
+            }
+            else if(this.choice == 1){
+                this.drawPipesRanking();
+            }
+            else if(this.choice ==2){
+                this.drawPipesManual();
             }
 
             this.drawChoiceBox();
@@ -94,6 +107,16 @@ class SceneMenu extends Phaser.Scene {
                 this.choice = 0;
             } else {
                 this.choice++;
+            }
+
+            if(this.choice == 0){
+                this.drawPipesInitial();
+            }
+            else if(this.choice == 1){
+                this.drawPipesRanking();
+            }
+            else if(this.choice ==2){
+                this.drawPipesManual();
             }
 
             this.drawChoiceBox();
@@ -126,15 +149,21 @@ class SceneMenu extends Phaser.Scene {
         this.choiceBox.lineStyle(3, 0xdf7919, 1);
         this.choiceBox.strokeRect(520, 285 + this.choice * 100, 250, 70);
     }
+
+    drawPipesGame()
+    {
+        this.add.image(788, 320, "bluePipe1");
+    }
+
     drawPipesInitial()
     {
-        this.add.image(788, 320, "redPipe1");
-        this.add.image(820, 320, "redPipe1");
-        this.add.image(852, 320, "redPipe1");
-        this.add.image(882, 323, "redPipe2");
+        this.add.image(788, 320, "bluePipe1");
+        this.add.image(820, 320, "bluePipe1");
+        this.add.image(852, 320, "bluePipe1");
+        this.add.image(882, 323, "bluePipe2");
 
-        this.add.image(884, 353, "redPipe5");
-        this.add.image(884, 387, "redPipe5");
+        this.add.image(884, 353, "bluePipe6");
+        this.add.image(884, 387, "bluePipe6");
 
         this.add.image(788, 420, "redPipe1");
         this.add.image(820, 420, "redPipe1");
@@ -148,18 +177,18 @@ class SceneMenu extends Phaser.Scene {
         this.add.image(852, 520, "redPipe1");
         this.add.image(882, 517, "redPipe3");
 
-        this.add.image(884, 420, "redPipe4");
-        this.add.image(916, 420, "redPipe1");
-        this.add.image(948, 420, "redPipe1");
-        this.add.image(980, 420, "redPipe1");
+        this.add.image(884, 420, "bluePipe4");
+        this.add.image(916, 420, "bluePipe1");
+        this.add.image(948, 420, "bluePipe1");
+        this.add.image(980, 420, "bluePipe1");
 
-        this.add.image(501, 320, "bluePipe1");
-        this.add.image(469, 320, "bluePipe1");
-        this.add.image(437, 320, "bluePipe1");
-        this.add.image(407, 323, "bluePipe5");
+        this.add.image(501, 320, "redPipe1");
+        this.add.image(469, 320, "redPipe1");
+        this.add.image(437, 320, "redPipe1");
+        this.add.image(407, 323, "redPipe6");
 
-        this.add.image(404, 353, "bluePipe6");
-        this.add.image(404, 387, "bluePipe6");
+        this.add.image(404, 353, "redPipe5");
+        this.add.image(404, 387, "redPipe5");
 
         this.add.image(501, 420, "bluePipe1");
         this.add.image(469, 420, "bluePipe1");
@@ -173,9 +202,114 @@ class SceneMenu extends Phaser.Scene {
         this.add.image(437, 520, "bluePipe1");
         this.add.image(407, 518, "bluePipe3");
 
-        this.add.image(404, 420, "bluePipe4");
-        this.add.image(371, 420, "bluePipe1");
-        this.add.image(338, 420, "bluePipe1");
-        this.add.image(305, 420, "bluePipe1");
+        this.add.image(404, 420, "redPipe4");
+        this.add.image(371, 420, "redPipe1");
+        this.add.image(338, 420, "redPipe1");
+        this.add.image(305, 420, "redPipe1");
     }
+
+    drawPipesRanking()
+    {
+        this.add.image(788, 320, "redPipe1");
+        this.add.image(820, 320, "redPipe1");
+        this.add.image(852, 320, "redPipe1");
+        this.add.image(882, 323, "redPipe2"); 
+
+        this.add.image(884, 353, "redPipe5");
+        this.add.image(884, 387, "redPipe5");
+
+        this.add.image(788, 420, "bluePipe1");
+        this.add.image(820, 420, "bluePipe1");
+        this.add.image(852, 420, "bluePipe1");
+
+        this.add.image(884, 486, "redPipe5");
+        this.add.image(884, 452, "redPipe5");
+
+        this.add.image(788, 520, "redPipe1");
+        this.add.image(820, 520, "redPipe1");
+        this.add.image(852, 520, "redPipe1");
+        this.add.image(882, 517, "redPipe3");
+
+        this.add.image(884, 420, "bluePipe4");
+        this.add.image(916, 420, "bluePipe1");
+        this.add.image(948, 420, "bluePipe1");
+        this.add.image(980, 420, "bluePipe1");
+
+        this.add.image(501, 320, "bluePipe1");
+        this.add.image(469, 320, "bluePipe1");
+        this.add.image(437, 320, "bluePipe1");
+        this.add.image(407, 323, "bluePipe5");
+
+        this.add.image(404, 353, "bluePipe6");
+        this.add.image(404, 387, "bluePipe6");
+
+        this.add.image(501, 420, "redPipe1");
+        this.add.image(469, 420, "redPipe1");
+        this.add.image(437, 420, "redPipe1");
+
+        this.add.image(404, 453, "bluePipe6");
+        this.add.image(404, 487, "bluePipe6");
+
+        this.add.image(501, 520, "bluePipe1");
+        this.add.image(469, 520, "bluePipe1");
+        this.add.image(437, 520, "bluePipe1");
+        this.add.image(407, 518, "bluePipe3");
+
+        this.add.image(404, 420, "redPipe4");
+        this.add.image(371, 420, "redPipe1");
+        this.add.image(338, 420, "redPipe1");
+        this.add.image(305, 420, "redPipe1");   
+    }
+    drawPipesManual(){
+        this.add.image(788, 320, "redPipe1");
+        this.add.image(820, 320, "redPipe1");
+        this.add.image(852, 320, "redPipe1");
+        this.add.image(882, 323, "redPipe2"); 
+
+        this.add.image(884, 353, "redPipe5");
+        this.add.image(884, 387, "redPipe5");
+
+        this.add.image(788, 420, "redPipe1");
+        this.add.image(820, 420, "redPipe1");
+        this.add.image(852, 420, "redPipe1");
+
+        this.add.image(884, 486, "bluePipe6");
+        this.add.image(884, 452, "bluePipe6");
+
+        this.add.image(788, 520, "bluePipe1");
+        this.add.image(820, 520, "bluePipe1");
+        this.add.image(852, 520, "bluePipe1");
+        this.add.image(882, 517, "bluePipe7");
+
+        this.add.image(884, 420, "bluePipe4");
+        this.add.image(916, 420, "bluePipe1");
+        this.add.image(948, 420, "bluePipe1");
+        this.add.image(980, 420, "bluePipe1");
+
+        this.add.image(501, 320, "bluePipe1");
+        this.add.image(469, 320, "bluePipe1");
+        this.add.image(437, 320, "bluePipe1");
+        this.add.image(407, 323, "bluePipe5");
+
+        this.add.image(404, 353, "bluePipe6");
+        this.add.image(404, 387, "bluePipe6");
+
+        this.add.image(501, 420, "bluePipe1");
+        this.add.image(469, 420, "bluePipe1");
+        this.add.image(437, 420, "bluePipe1");
+
+        this.add.image(404, 453, "redPipe5");
+        this.add.image(404, 487, "redPipe5");
+
+        this.add.image(501, 520, "redPipe1");
+        this.add.image(469, 520, "redPipe1");
+        this.add.image(437, 520, "redPipe1");
+        this.add.image(407, 518, "redPipe7");
+
+        this.add.image(404, 420, "redPipe4");
+        this.add.image(371, 420, "redPipe1");
+        this.add.image(338, 420, "redPipe1");
+        this.add.image(305, 420, "redPipe1");
+    }
+
 }
